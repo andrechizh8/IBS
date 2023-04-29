1808af2b8da804ee948be641a5a5647e3c12c96d
andrechizh8 <andrechizh.ru@yandex.ru>
2023-04-30 00:03:21 +0300

    refactoring

--- model/ui/pages/base_page.py
+++ model/ui/pages/base_page.py
@@ -15,14 +15,14 @@ class BasePage:
     def open(self):
         self.driver.get(self.url)
 
-    def element_is_present(self, locator, timeout=5):
+    def element_is_present(self, locator, timeout=3):
         return wait(self.driver, timeout).until(EC.presence_of_element_located(locator))
 
-    def element_is_visible(self, locator, timeout=5):
+    def element_is_visible(self, locator, timeout=3):
         self.go_to_element(self.element_is_present(locator))
         return wait(self.driver, timeout).until(EC.visibility_of_element_located(locator))
 
-    def element_is_clickable(self, locator, timeout=5):
+    def element_is_clickable(self, locator, timeout=3):
         return wait(self.driver, timeout).until(EC.element_to_be_clickable(locator))
 
     def go_to_element(self, element):
